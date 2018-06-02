f795dda3db854cebf9aabc2228b6db25577f8b06
eckmLJE <lucas.eckman@gmail.com>
2018-06-02 10:14:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+
+let currentUser = "Lucas";
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + '!';
+const excitedWelcomeMessage = (welcomeMessage + currentUser).toUpperCase() + '!';
+const shortGreeting = "Welcome, " + currentUser.charAt(0) + '!'
